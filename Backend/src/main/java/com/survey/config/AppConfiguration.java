40bdfa8390b1ebfc27116d7ac41d95a5909b4e82
artur <arturstorm@gmail.com>
2016-03-11 18:18:15 +0200

    InitialCommit

---
+++ Backend/src/main/java/com/survey/config/AppConfiguration.java
@@ -0,0 +1,16 @@
+package com.survey.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author Artur Myseliuk
+ */
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.survey")
+public class AppConfiguration extends WebMvcConfigurerAdapter {
+}
