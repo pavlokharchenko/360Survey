33389783daedf35a27bcc04027a33c86cea061fc
artur <arturstorm@gmail.com>
2016-03-12 15:10:13 +0200

    Fix conflict

--- Backend/src/main/java/com/survey/config/WebAppConfiguration.java
+++ Backend/src/main/java/com/survey/config/WebAppConfiguration.java
@@ -12,6 +12,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.survey")
-public class AppConfiguration extends WebMvcConfigurerAdapter {
+public class WebAppConfiguration extends WebMvcConfigurerAdapter {
 
 }
